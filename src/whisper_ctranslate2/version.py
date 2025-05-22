3800b076592cfaf1a6210fdc1aaf18b10fd28acf
Jordi Mas <jmas@softcatala.org>
2025-05-22 19:03:45 +0200

    Merge versioning, keep same dh version

--- src/whisper_ctranslate2/version.py
+++ src/whisper_ctranslate2/version.py
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 __version__ = "0.5.0+dh.4"
 =======
 __version__ = "0.5.2"
 >>>>>>> 34da493 (Bump version to 0.5.2)
+=======
+__version__ = "0.5.3"
+>>>>>>> 0f6686f (Bump version to 0.5.3)
