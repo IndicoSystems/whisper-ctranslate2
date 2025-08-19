bb6c522f078375b5dbef09f45c6ad7407fbfb3bd
Alexander Skage <alexander@davidhorn.com>
2025-08-19 14:53:31 +0000

    Script for downloading the required models for offline diarization

---
+++ src/whisper_ctranslate2/download_diarization_model.py
@@ -0,0 +1,37 @@
+from huggingface_hub import snapshot_download
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+# Move up two directories to the root of the project
+os.chdir("../..")
+
+HF_TOKEN = os.environ["HF_TOKEN"]
+
+print(HF_TOKEN)
+
+# Pin exact revisions for reproducibility (replace with commit SHAs you trust)
+# snapshot_download(
+#     "pyannote/segmentation-3.0",
+#     local_dir="/opt/models/pyannote/segmentation-3.0",
+#     token=HF_TOKEN,
+#     revision="main"  # ideally a commit sha
+# )
+
+#model_dir = "./model_dir/pyannote/speaker-diarization-3.1"
+
+#if not os.path.exists(model_dir):
+#   os.makedirs(model_dir)
+print(os.getenv("HF_CACHE_DIR"))
+
+snapshot_download("pyannote/segmentation-3.0", token=HF_TOKEN, local_dir="./model_dir/pyannote/segmentation-3.0", revision="main")
+snapshot_download("pyannote/wespeaker-voxceleb-resnet34-LM", token=HF_TOKEN, local_dir="./model_dir/pyannote/wespeaker-voxceleb-resnet34-LM", revision="main")
+
+snapshot_download(
+    "pyannote/speaker-diarization-3.1",
+    token=HF_TOKEN,
+    revision="main",
+    local_dir="./model_dir/pyannote/speaker-diarization-3.1"
+)
