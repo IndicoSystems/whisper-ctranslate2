dd2ed8fb872abfab85e6479977f3e76032e0877f
Jordi Mas <jmas@softcatala.org>
2025-10-26 18:15:09 +0100

    Resolve version conflict

--- src/whisper_ctranslate2/version.py
+++ src/whisper_ctranslate2/version.py
@@ -1,6 +1,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 __version__ = "0.5.0+dh.4"
 =======
 __version__ = "0.5.2"
@@ -11,3 +12,6 @@ __version__ = "0.5.3"
 =======
 __version__ = "0.5.4"
 >>>>>>> 8525eb7 (Bump version to 0.5.4)
+=======
+__version__ = "0.5.5"
+>>>>>>> 47a950a (Bump version to 0.5.5)
