6d2aa2be7f0a6eab662fd7880a27743f34fd090d
Alexander Skage <alexander@davidhorn.com>
2025-11-26 21:14:16 +0100

    Fix version

--- src/whisper_ctranslate2/version.py
+++ src/whisper_ctranslate2/version.py
@@ -1,17 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 __version__ = "0.5.0+dh.4"
-=======
-__version__ = "0.5.2"
->>>>>>> 34da493 (Bump version to 0.5.2)
-=======
-__version__ = "0.5.3"
->>>>>>> 0f6686f (Bump version to 0.5.3)
-=======
-__version__ = "0.5.4"
->>>>>>> 8525eb7 (Bump version to 0.5.4)
-=======
-__version__ = "0.5.5"
->>>>>>> 47a950a (Bump version to 0.5.5)
